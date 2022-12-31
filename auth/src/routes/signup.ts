4c50a8dc156e298f2e317ef3cb43047d2bdd1a27
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-31 22:51:39 +0530

    signup with validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-	res.send("hellu lolu");
-});
+router.post(
+	"/api/users/signup",
+	[
+		body("email").isEmail().withMessage("Email must be valid"),
+		body("password")
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.withMessage("Password must be between 4 and 20 characters"),
+	],
+	(req: Request, res: Response) => {
+		// error handling
+		const errors = validationResult(req);
+		// if there are errors then return the array of errors
+		if (!errors.isEmpty()) {
+			throw new Error("invalid email or pass");
+		}
+
+		const { email, password } = req.body;
+
+		//create a user as email and pass are validated
+		throw new Error("db down");
+		res.send({});
+	}
+);
 
 export { router as signupRouter };
