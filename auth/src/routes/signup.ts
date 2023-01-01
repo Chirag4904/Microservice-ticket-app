7edb2bb9da4719daa1ea285a11a5c70914736893
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-01 16:39:10 +0530

    using error handlers

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -17,13 +19,13 @@ router.post(
 		const errors = validationResult(req);
 		// if there are errors then return the array of errors
 		if (!errors.isEmpty()) {
-			throw new Error("invalid email or pass");
+			throw new RequestValidationError(errors.array());
 		}
 
 		const { email, password } = req.body;
 
 		//create a user as email and pass are validated
-		throw new Error("db down");
+		throw new DatabaseConnectionError();
 		res.send({});
 	}
 );
