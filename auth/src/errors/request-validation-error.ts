10af64c85d7428caa2217363d5525913ebbd8e1d
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-01 21:25:17 +0530

    all errors type extends customError abstract

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,13 +1,21 @@
 import { ValidationError } from "express-validator";
-
-export class RequestValidationError extends Error {
+import { CustomError } from "./custom-error";
+export class RequestValidationError extends CustomError {
+	statusCode = 400;
 	errors: ValidationError[];
 	constructor(errors: ValidationError[]) {
-		super();
+		super("Invalid request parameters");
 		this.errors = errors;
 
 		// Only because we are extending a built in class
 		Object.setPrototypeOf(this, RequestValidationError.prototype);
+	}
+
+	serializeErrors() {
+		//returns array of errors
+		return this.errors.map((error) => {
+			return { message: error.msg, field: error.param };
+		});
 	}
 }
 
