f93e841f009c46011b8cc6b4b6af87d7ed31057a
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-04 23:26:24 +0530

    sign in tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Fail when email doesnt exist", async () => {
+	await request(app)
+		.post("/api/users/signin")
+		.send({
+			email: "test@test.com",
+			password: "password",
+		})
+		.expect(400);
+});
+
+it("Fail when password is wrong", async () => {
+	await request(app)
+		.post("/api/users/signup")
+		.send({
+			email: "test@test.com",
+			password: "password",
+		})
+		.expect(201);
+
+	await request(app)
+		.post("/api/users/signin")
+		.send({
+			email: "test@test.com",
+			password: "passw",
+		})
+		.expect(400);
+});
+
+it("Response with cookie when valid credentials", async () => {
+	await request(app)
+		.post("/api/users/signup")
+		.send({
+			email: "test@test.com",
+			password: "password",
+		})
+		.expect(201);
+
+	const response = await request(app)
+		.post("/api/users/signin")
+		.send({
+			email: "test@test.com",
+			password: "password",
+		})
+		.expect(200);
+
+	expect(response.get("Set-Cookie")).toBeDefined();
+});
