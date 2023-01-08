482d274fd98ce7c5ce05cc3d7489b95a4d69c0ec
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-08 22:59:29 +0530

    logic moved to common config axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-	console.log(currentUser);
-	console.log("hello");
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are not signed in</h1>
+	);
 };
 //http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-LandingPage.getInitialProps = async ({ req }) => {
-	// console.log(req.headers)
-
-	if (typeof window === "undefined") {
-		//we are on server so make req to ingress service   //http://SERVICE-NAME.NAMESPACE.svc.cluster.local
-		const response = await axios.get(
-			"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-			{
-				headers: req.headers,
-			}
-		);
-
-		return response.data;
-	} else {
-		//we are on browser
-		const response = await axios.get("/api/users/currentuser");
-		return response.data;
-	}
-	return {};
+LandingPage.getInitialProps = async (context) => {
+	// console.log(context);
+	const response = await buildClient(context).get("/api/users/currentuser");
+	return response.data;
 };
 
 export default LandingPage;
