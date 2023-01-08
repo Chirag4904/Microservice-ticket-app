2cf0ab1aac977dfe2eb342a453fe4c7ff683d5b7
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-08 18:04:44 +0530

    handling ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
-	return <div>Hi there!!!</div>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser);
+	console.log("hello");
 };
+//http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+LandingPage.getInitialProps = async ({ req }) => {
+	// console.log(req.headers)
+
+	if (typeof window === "undefined") {
+		//we are on server so make req to ingress service   //http://SERVICE-NAME.NAMESPACE.svc.cluster.local
+		const response = await axios.get(
+			"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+			{
+				headers: req.headers,
+			}
+		);
+
+		return response.data;
+	} else {
+		//we are on browser
+		const response = await axios.get("/api/users/currentuser");
+		return response.data;
+	}
+	return {};
+};
+
+export default LandingPage;
