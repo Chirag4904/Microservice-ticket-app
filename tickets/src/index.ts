54ac318ac7e4d72e82213acd9c58c06695f6e169
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-12 19:01:55 +0530

    initial setup

---
+++ tickets/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const startServer = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error("JWT Key must be defined");
+	}
+
+	if (!process.env.MONGO_URI) {
+		throw new Error("MONGO_URI must be defined");
+	}
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("connected to mongodb");
+	} catch (err) {
+		console.log(err);
+	}
+
+	app.listen(3000, () => {
+		console.log("Listening on port 3000");
+	});
+};
+
+startServer();
