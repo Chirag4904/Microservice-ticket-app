62a0aebaffa702ef766344ec4b989c90a937745f
chiggy69 <chiragaggarwal69@gmail.com>
2023-01-24 20:06:26 +0530

    🎨 help typescript to identify errors easily

--- nats-testing/src/events/base-listener.ts
+++ nats-testing/src/events/base-listener.ts
@@ -1,9 +1,16 @@
 import { Message, Stan } from "node-nats-streaming";
-export abstract class Listener {
+import { Subjects } from "./subjects";
+
+interface Event {
+	subject: Subjects;
+	data: any;
+}
+
+export abstract class Listener<T extends Event> {
 	//abstract property and method must be defined in the child class
-	abstract subject: string;
+	abstract subject: T["subject"];
 	abstract queueGroupName: string;
-	abstract onMessage(data: any, msg: Message): void;
+	abstract onMessage(data: T["data"], msg: Message): void;
 
 	private client: Stan;
 	protected ackWait = 5 * 1000;
