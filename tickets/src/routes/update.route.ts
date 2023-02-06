d0091df90d78ab04e82d814746ed33a7abfe5cd3
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-07 00:14:14 +0530

    ✨ Reserving a ticket

--- tickets/src/routes/update.route.ts
+++ tickets/src/routes/update.route.ts
@@ -5,6 +5,7 @@ import {
 	validateRequest,
 	NotFoundError,
 	NotAuthorizedError,
+	BadRequestError,
 } from "@caticket/common";
 import { Ticket } from "../models/ticket.model";
 import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
@@ -25,6 +26,10 @@ router.put(
 		const ticket = await Ticket.findById(req.params.id);
 		if (!ticket) {
 			throw new NotFoundError();
+		}
+
+		if (ticket.orderId) {
+			throw new BadRequestError("Cannot edit a reserved ticket");
 		}
 
 		if (ticket.userId !== req.currentUser!.id) {
